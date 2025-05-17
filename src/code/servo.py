ec634efd7b37779c0765c7dcb495abcb28061158
4D-6429 <demoruzi@gmail.com>
2025-05-17 11:26:31 +0300

    Added required libraries

---
+++ src/code/servo.py
@@ -0,0 +1,20 @@
+from adafruit_servokit import ServoKit
+import time
+kit = ServoKit(channels=16)
+
+kit.servo[1].set_pulse_width_range(525, 2475)
+kit.servo[2].set_pulse_width_range(525, 2475)
+
+kit.servo[1].angle=120
+kit.servo[2].angle=120
+
+kit.servo[3].angle=120
+kit.servo[4].angle=120
+
+time.sleep(3)
+
+kit.servo[1].angle=90
+kit.servo[2].angle=90
+
+kit.servo[3].angle=90
+kit.servo[4].angle=90
--- src/servo.py
+++
@@ -1,6 +0,0 @@
-from adafruit_servokit import ServoKit
-import time
-kit = ServoKit(channels=16)
-kit.servo[0].angle=0
-time.sleep(3)
-kit.servo[0].angle=180
